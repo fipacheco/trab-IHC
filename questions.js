a31ed66e61ce616c5323360ea24331f124c2ad2a
fipacheco <filipesmpacheco@gmail.com>
2022-12-05 14:23:29 -0300

    versao final

--- questions.js
+++ questions.js
@@ -1,34 +1,39 @@
 export default [
     {
-      question: "Quanto é 1 + 2?",
+      question: "Uma pessoa pretende obter a habilitação necessária para conduzir um ônibus escolar. Qual a categoria da CNH correspondente a esse tipo de transporte, conforme o Código de Trânsito Brasileiro?",
       answers: [
-        { option: "3", correct: true },
-        { option: "2", correct: false },
-        { option: "4", correct: false },
+        { option: "Categoria D", correct: true },
+        { option: "Categoria A", correct: false },
+        { option: "Categoria C", correct: false },
+        { option: "Categoria B", correct: false },
       ],
     },
     {
-      question: "Quanto é 5 x 2?",
+      question: "Para habilitar-se nas categorias D e E, ou para conduzir veículo de transporte coletivo de passageiros, de escolares, de emergência ou de produto perigoso, o candidato deverá preencher, entre outros, o requisito de não ter cometido nenhuma infração gravíssima nos últimos:",
       answers: [
-        { option: "9", correct: false },
-        { option: "10", correct: true },
-        { option: "11", correct: false },
+        { option: "6 meses", correct: false },
+        { option: "12 meses", correct: true },
+        { option: "18 meses", correct: false },
+        { option: "24 meses", correct: false },
       ],
     },
     {
-      question: "Quanto é 10 x 8?",
+      question: "Os condutores habilitados nas categorias C, D e E, com a Carteira Nacional de Habilitação com validade de 5 (cinco) anos, deverão fazer o exame toxicológico previsto no artigo 148-A e parágrafo 2º, no prazo de:",
       answers: [
-        { option: "800", correct: false },
-        { option: "90", correct: false },
-        { option: "80", correct: true },
+        { option: "1 ano", correct: false },
+        { option: "1 ano e 6 meses", correct: false },
+        { option: "2 anos", correct: false },
+        { option: "2 anos e 6 meses", correct: true },
       ],
     },
     {
-      question: "Quanto é 7 x 7?",
+      question: "Para conduzir um veículo bi-articulado (Onibus), um condutor deverá ter a CNH na categoria mínima:",
       answers: [
-        { option: "70", correct: false },
-        { option: "56", correct: false },
-        { option: "49", correct: true },
+        { option: "Categoria D", correct: false },
+        { option: "Categoria C", correct: false },
+        { option: "Categoria E", correct: true },
+        { option: "Categoria D Especial", correct: false },
+        
       ],
     },
   ];
